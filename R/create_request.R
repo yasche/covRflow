d3b8cd0762a720e7a68d05d0b8c20d4e32027d01
yasche <172521093+yasche@users.noreply.github.com>
2025-06-21 18:25:17 +0200

    Add example_albums and documentation for example_albums and create_request_discogs

--- R/create_request.R
+++ R/create_request.R
@@ -1,15 +1,23 @@
 #' Create requests to retrieve covers from the Discogs API
 #'
 #' @param data A tibble with the following columns: \itemize{
-##'  \item{release_title} {Stuff}
-##'  \item{artist} {Stuff}
-##' }
-#' @param access_token
+##'  \item{`release_title`} {The title of the release, for example an album title.}
+##'  \item{`artist`} {The artist who created the release.}
+##'  \item{`year`} {The year of the release.}
+##'  \item{`format`} {The format of the release. Examples include "album", "LP", "CD", "Vinyl".}
+##' } See
+#' @param access_token A string containing your Discogs API token. Unfortunately, you have to identify yourself with your personal token to use the functionality necessery to search for album covers. If you do not have a token yet, go to <https://www.discogs.com/settings/developers> and sign in to your Discogs account (or create a new one for free) and click on "Generate new token".
 #'
-#' @returns
+#' @returns A tibble with a new list column called `request`.
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' example_albums |>
+#'   create_request_discogs(access_token = "thisisasecret") |>
+#'   retrieve_covers_discogs() |>
+#'   covRflow()
+#' }
 create_request_discogs <- function(data, access_token) {
   data |>
     dplyr::mutate(request = purrr::pmap(list(.data$release_title, .data$artist, .data$year, .data$format), discogs_create_request, .env$access_token))
---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Some randomly selected albums
+#'
+#' Albums were selected from Apple Music's top 100 albums of all time <https://100best.music.apple.com/us>.
+#'
+#' @format ## `example_albums`
+#' A tibble with 7 rows and 4 columns:
+#' \describe{
+#'   \item{release_title}{The album title.}
+#'   \item{artist}{The performing artist.}
+#'   \item{year}{The year of release.}
+#'   \item{format}{The format. In this case all are albums but cut also be "LP" or more specific, for example "CD" or "Vinyl".}
+#' }
+#' @source <https://100best.music.apple.com/us>
+"example_albums"
